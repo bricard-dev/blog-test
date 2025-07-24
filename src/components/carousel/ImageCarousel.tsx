04fa6656baa20e50954ee29fac8131938d1036db
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-07-24 11:42:34 +0200

    Refactor

--- src/components/carousel/ImageCarousel.tsx
+++ src/components/carousel/ImageCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Article } from "@/lib/definitions";
+import { carouselSlides } from "@/lib/data";
 import { cn } from "@/lib/utils";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
@@ -14,45 +14,6 @@ import {
   CarouselContent,
   CarouselItem,
 } from "../ui/carousel";
-
-const articles: Article[] = [
-  {
-    id: 1,
-    image: {
-      src: "https://images.unsplash.com/photo-1509840841025-9088ba78a826?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Forest path",
-    },
-    category: "Espagne",
-    title: "Séville, entre flamenco et azulejos : mon carnet de voyage andalou",
-    date: "12 juillet 2025",
-    description:
-      "De retour d’un séjour envoûtant à Séville, je vous partage mes impressions, coups de coeur et découvertes au fil des ruelles pavées, des patios fleuries et des monuments baignés de soleil.",
-  },
-  {
-    id: 2,
-    image: {
-      src: "https://images.unsplash.com/photo-1515268064940-5150b7c29f35?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Mountain landscape",
-    },
-    category: "France",
-    title:
-      "À la conquête des Alpes : une randonnée inoubliable au cœur des sommets",
-    date: "27 juin 2025",
-    description:
-      "Découverte d’un itinéraire époustouflant à travers vallées verdoyantes, cols panoramiques et sommets majestueux des Alpes françaises.",
-  },
-  {
-    id: 3,
-    image: {
-      src: "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Mountain lake",
-    },
-    category: "Wellness",
-    title: "Serene Waters Await",
-    date: "July 14, 2025",
-    description: "Find peace by crystal clear alpine lakes",
-  },
-];
 
 export default function ImageCarousel() {
   const [api, setApi] = useState<CarouselApi>();
@@ -79,15 +40,15 @@ export default function ImageCarousel() {
   return (
     <Carousel opts={{ loop: true }} plugins={[autoplay]} setApi={setApi}>
       <CarouselContent className="ml-0">
-        {articles.map((article) => (
-          <CarouselItem key={article.id} className="group h-[500px] pl-0">
+        {carouselSlides.map((carouselSlide) => (
+          <CarouselItem key={carouselSlide.id} className="group h-[500px] pl-0">
             <Link
-              href={`/article/${article.id}`}
+              href={`/article/${carouselSlide.id}`}
               className="relative block h-full"
             >
               <Image
-                src={article.image.src}
-                alt={article.image.alt}
+                src={carouselSlide.image.src}
+                alt={carouselSlide.image.alt}
                 fill
                 priority
                 className="object-cover"
@@ -100,15 +61,18 @@ export default function ImageCarousel() {
 
               <div className="absolute right-0 bottom-[74px] left-0 z-30 mx-auto max-w-5xl">
                 <div className="max-w-2xl px-6 select-none">
-                  <Category category={article.category} className="mb-4" />
+                  <Category
+                    category={carouselSlide.category}
+                    className="mb-4"
+                  />
                   <h1 className="mb-4 font-mono text-4xl font-bold text-white">
-                    {article.title}
+                    {carouselSlide.title}
                   </h1>
                   <div className="flex gap-4 font-mono text-sm leading-6 text-white">
-                    <p className="shrink-0">{article.date}</p>
+                    <p className="shrink-0">{carouselSlide.date}</p>
                     <div className="border" />
                     <p className="line-clamp-3 md:line-clamp-4">
-                      {article.description}
+                      {carouselSlide.description}
                     </p>
                   </div>
                 </div>
@@ -121,7 +85,7 @@ export default function ImageCarousel() {
       {/* Dot */}
       <div className="absolute right-0 bottom-10 left-0 z-30 mx-auto max-w-5xl px-6">
         <div className="flex gap-2.5">
-          {Array.from({ length: articles.length }, (_, index) => (
+          {Array.from({ length: carouselSlides.length }, (_, index) => (
             <Button
               key={index}
               onClick={() => api?.scrollTo(index)}
---
+++ src/lib/data.ts
@@ -0,0 +1,42 @@
+import { Article } from "./definitions";
+
+export const carouselSlides: Article[] = [
+  {
+    id: 1,
+    image: {
+      src: "https://images.unsplash.com/photo-1509840841025-9088ba78a826?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Forest path",
+    },
+    category: "Espagne",
+    title: "Séville, entre flamenco et azulejos : mon carnet de voyage andalou",
+    date: "12 juillet 2025",
+    description:
+      "De retour d’un séjour envoûtant à Séville, je vous partage mes impressions, coups de coeur et découvertes au fil des ruelles pavées, des patios fleuries et des monuments baignés de soleil.",
+  },
+  {
+    id: 2,
+    image: {
+      src: "https://images.unsplash.com/photo-1515268064940-5150b7c29f35?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Mountain landscape",
+    },
+    category: "France",
+    title:
+      "À la conquête des Alpes : une randonnée inoubliable au cœur des sommets",
+    date: "27 juin 2025",
+    description:
+      "Découverte d’un itinéraire époustouflant à travers vallées verdoyantes, cols panoramiques et sommets majestueux des Alpes françaises.",
+  },
+  {
+    id: 3,
+    image: {
+      src: "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Mountain lake",
+    },
+    category: "Wellness",
+    title:
+      "Immersion en forêt andalouse : randonnée ressourçante près de Séville",
+    date: "02 juin 2025",
+    description:
+      "Explorez la beauté préservée des forêts sévillanes à travers un sentier ombragé entre pins, chênes-lièges et parfums méditerranéens.",
+  },
+];
