747cda44f94637b3f3fbd855223fb70bc956c1d7
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-08-01 17:26:53 +0200

    fix footer max width

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="bg-primary mx-auto max-w-5xl px-6 py-10">
-      <p className="text-center text-xs text-white">
-        © 2025 Blog. All rights reserved
-      </p>
+    <footer className="bg-primary">
+      <div className="mx-auto max-w-5xl px-6 py-10">
+        <p className="text-center text-xs text-white">
+          © 2025 Blog. All rights reserved
+        </p>
+      </div>
     </footer>
   );
 }
